d8dd57d46e029c2e546229dc9c9e279245c2f899
Spencer Lindemuth <lindemut@gmail.com>
2019-02-26 02:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musician, instrument){
+  var bar = []
+  for(var i = 0; i <= musician.length(); i++){
+    var concat = musician[i] + instrument[i]
+    bar = bar.push(concat)
+  }
+  return bar
+}
